aed01deec46e4a281728cf0ea98f0c486f9d2a56
Mellostar25 <harshshukla1912@gmail.com>
2022-05-21 17:00:37 -0400

    map stuff

--- index.js
+++ index.js
@@ -4,6 +4,60 @@
 
 // searchBar.addEventListener('keyup', )
 
+const apikey = "pk.eyJ1IjoianA4NDEzMzEiLCJhIjoiY2wzZjEwMGE0MDBiaTNla2JsZnB0M3RmdSJ9.ZLQyQW6BpT7i2PcIG2beOw"
+let pos = [43.53271, -80.22747];
+latitude = []
+longitude = []
+
+// let mymap = L.map('map').setView([pos[0], pos[1]], 13);
+
+document.querySelector('.find-state').addEventListener('click', () => {
+    const status = document.querySelector('.status');
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        pos = [position.coords.latitude, position.coords.longitude];
+        console.log(pos);
+        const mymap = L.map('map').setView([pos[0], pos[1]], 13);
+
+        console.log("longitude: ", longitude)
+        console.log("latitude: ", latitude)
+        
+        //ALL THE MARKERS COME IN HERE
+        for(i = 0; i<longitude.length; i++){
+
+        var marker = L.marker(latitude[i], longitude[i]).addTo(mymap);
+        marker.bindPopup(ARRAYKEYWORDS[i]);
+
+        }
+
+
+
+
+        //DO NOT TOUCH --
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: apikey
+        }).addTo(mymap);
+        //-- DO NOT TOUCH ENDS
+    }, () => {
+        status.textConent = "FAIL"
+        pos = [43.53271, -80.22747];
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: apikey
+    }).addTo(mymap);
+
+    });
+    // L.map('map').setView([pos[0], pos[1]], 13).addTo(mymap);
+});
+
+
 async function getEvents() {
   let posts = await fetch("http://34.125.42.33:8080/api/events/all", {
     mode: "cors",
@@ -56,6 +110,10 @@ async function getEvents() {
         tempDesc.className = "description";
         tempDesc.innerHTML = posts[i].description;
         document.getElementById("post" + i).appendChild(tempDesc);
+      }
+      if (posts[i].lat && posts[i].long){
+        latitude.push(posts[i].lat)
+        longitude.push(posts[i].long)
       }
     }
   }
