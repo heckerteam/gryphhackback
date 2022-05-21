0e30d784a1d20df3fea438df68f426e2629945f0
Mellostar25 <harshshukla1912@gmail.com>
2022-05-21 18:51:43 -0400

    fix sending data to api

--- gryphhacks/service.js
+++ gryphhacks/service.js
@@ -1,48 +1,43 @@
-var latitude = [];
-var longitude = [];
-
-async function getCords(address){
-  let data = await fetch('http://api.positionstack.com/v1/forward?access_key=d089bf01bca2096c3dd0a8c6bdb838ac&query=' + address)
-    .then(response => response.json())
-    .then(res => res.data[0])
-    .then(data => data)
-    let out = [data.latitude, data.longitude]
-    // console.log(getCords("brampton")[0])
-    // console.log(longitude)
-    latitude.push(out[0])
-    return out
-    
+async function getCords(address) {
+  let data = await fetch(
+    "http://api.positionstack.com/v1/forward?access_key=d089bf01bca2096c3dd0a8c6bdb838ac&query=" +
+      address
+  )
+    .then((response) => response.json())
+    .then((res) => res.data[0])
+    .then((d) => d);
+  return [data.latitude, data.longitude];
 }
 
-getCords("brampton")
-console.log(latitude)
-console.log(longitude)
-
-function sendData(){
-    let nameHtml = document.getElementById("name")
-    let sTime = document.getElementById('stime')
-    let eTime = document.getElementById("eTime")
-    let address = document.getElementById("address")
-    let desc = document.getElementById("desc")
-    fetch('apistuff', {
-        method: 'POST',
-        body: JSON.stringify({
-            name: nameHtml,
-            time_start: sTime,
-            time_end: eTime,
-            location: address,
-            latitude: getCords(address)[0],
-            longitude: getCords(address)[1],
-            description: desc
-        })
-    }).then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-        return Promise.reject(response);
-    }).then(function (data) {
-        console.log(data);
-    }).catch(function (error) {
-        console.warn('Something went wrong.', error);
+async function sendData() {
+  let nameHtml = document.getElementById("name");
+  let sTime = document.getElementById("stime");
+  let eTime = document.getElementById("eTime");
+  let address = document.getElementById("address");
+  let desc = document.getElementById("desc");
+  const [lat, long] = await (await getCords(address));
+  fetch("apistuff", {
+    method: "POST",
+    body: JSON.stringify({
+      name: nameHtml,
+      time_start: sTime,
+      time_end: eTime,
+      location: address,
+      latitude: lat,
+      longitude: long,
+      description: desc,
+    }),
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.warn("Something went wrong.", error);
     });
 }
