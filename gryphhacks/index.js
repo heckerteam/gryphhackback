442109b6254f3f8eb63cc09882236a020a0276ec
Mellostar25 <harshshukla1912@gmail.com>
2022-05-21 14:48:12 -0400

    Displaying Posts from API

--- gryphhacks/index.js
+++ gryphhacks/index.js
@@ -1,49 +1,63 @@
 //Search Bar
 
-var searchBar = document.getElementById('search');
+// var searchBar = document.getElementById('search');
 
-searchBar.addEventListener('keyup', )
+// searchBar.addEventListener('keyup', )
 
+async function getEvents() {
+  let posts = await fetch("http://34.125.42.33:8080/api/events/all", {
+    mode: "cors",
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      return res;
+    });
 
-function getPosts(){
-    if(posts.length>0){
-        for (i = 0; i < posts.length; i++) {
+  console.log(posts[0]);
+  if (posts.length > 0) {
+    for (i = 0; i < posts.length; i++) {
+      temp = document.createElement("div");
+      temp.className = "post";
+      temp.id = "post" + i;
+      document.getElementById("main").appendChild(temp);
 
-            temp  = document.createElement('div')
-            temp.className = "post"
-            temp.id = "post" + i
-            document.getElementById("main").appendChild(temp)
+      tempPosition = document.createElement("h2");
+      tempPosition.className = "position"
+      tempPosition.innerHTML = posts[i].name;
+      document.getElementById("post" + i).appendChild(tempPosition);
 
-            tempPosition = document.createElement('h2')
-            tempPosition.innerHTML = posts[i][0]
-            document.getElementById("post" + i).appendChild(tempPosition)
-
-            if(posts[i][1]){
-                tempService = document.createElement('h2')
-                tempService.className = "service"
-                tempService.innerHTML = posts[i][1]
-                document.getElementById("post" + i).appendChild(tempService)
-            }
-            if(posts[i][4]){
-                tempLocation = document.createElement('h2')
-                tempLocation.className = "location"
-                tempLocation.innerHTML = posts[i][4]
-                document.getElementById("post" + i).appendChild(tempLocation)
-            }
-            if(posts[i][2] && posts[i][3]){
-                tempTime = document.createElement('h2')
-                tempTime.className = "time"
-                var Sdate = new Date(posts[i][2])
-                var Edate = new Date(posts[i][3])
-                tempTime.innerHTML = Sdate.getHours() + ":" + Sdate.getMinutes() + " - " + Edate.getHours() + ":" + Edate.getMinutes()
-                document.getElementById("post" + i).appendChild(tempTime)
-            }
-            if(posts[i][5]){
-                tempDesc = document.createElement('p')
-                tempDesc.className = "description"
-                tempDesc.innerHTML = posts[i][4]
-                document.getElementById("post" + i).appendChild(tempDesc)
-            }
-        }
+      if (posts[i].location) {
+        tempLocation = document.createElement("h2");
+        tempLocation.className = "location";
+        tempLocation.innerHTML = posts[i].location;
+        document.getElementById("post" + i).appendChild(tempLocation);
+      }
+      if (posts[i].time_start && posts[i].time_end) {
+        tempTime = document.createElement("h2");
+        tempTime.className = "timings";
+        var Sdate = new Date(posts[i].time_start);
+        var Edate = new Date(posts[i].time_end);
+        tempTime.innerHTML =
+          Sdate.getHours() +
+          ":" +
+          Sdate.getMinutes() +
+          " - " +
+          Edate.getHours() +
+          ":" +
+          Edate.getMinutes();
+        document.getElementById("post" + i).appendChild(tempTime);
+      }
+      if (posts[i].description) {
+        tempDesc = document.createElement("p");
+        tempDesc.className = "description";
+        tempDesc.innerHTML = posts[i].description;
+        document.getElementById("post" + i).appendChild(tempDesc);
+      }
     }
+  }
 }
+
