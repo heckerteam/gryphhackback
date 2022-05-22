4cf657e19cd4cf625775569726c029875637c3a0
Christian <60714838+Shadow2ube@users.noreply.github.com>
2022-05-22 23:30:44 +0000

    kill me

--- gryphhacks/account.js
+++ gryphhacks/account.js
@@ -13,7 +13,7 @@ async function signUp() {
   };
   console.log(out);
 
-  fetch("https://api.sevahub.tech:8080/api/signup", {
+  fetch("http://api.sevahub.tech:8080/api/signup", {
     method: "POST",
     body: JSON.stringify(out),
   })
